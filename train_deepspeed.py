8879b9e50b498093da7bd6dc55acfcd4e20d7d5b
sonder <3274373235@qq.com>
2026-05-15 08:36:28 +0800

    更新 train_deepspeed.py

--- train_deepspeed.py
+++ train_deepspeed.py
@@ -1,4 +1,3 @@
-
 from logger import Logger
 from model import modeling_miniphi3, configuation_miniPhi3
 from datasets import Dataset, load_dataset
@@ -29,32 +28,12 @@ log = Logger('pretraun', save2file=True,
 class miniPhi3TrainerCallback(TrainerCallback):
     log_cnt = 0
 
-    def on_log(
-        self,
-        args: TrainingArguments,
-        state: TrainerState,
-        control: TrainerControl,
-        **kwargs,
-    ):
-        """
-        在打印 n 次日志后清除cuda缓存，适合低显存设备，能防止OOM
-        """
+    def on_log(self, args: TrainingArguments, state: TrainerState, control: TrainerControl, **kwargs):
         self.log_cnt += 1
         if self.log_cnt % 2 == 0:
             torch.cuda.empty_cache()
 
-    def on_epoch_end(
-        self,
-        args: TrainingArguments,
-        state: TrainerState,
-        control: TrainerControl,
-        **kwargs,
-    ):
-        """
-        在on_epoch_end时保存一次模型。
-        TrainingArguments的 save_strategy 中 epoch 和 steps 不兼容。要实现每隔 save_steps 步保存一次检查点，考虑到磁盘空间大小，最多只保存最近3个检查点。
-        """
-        # 设置should_save=True并返回即可
+    def on_epoch_end(self, args: TrainingArguments, state: TrainerState, control: TrainerControl, **kwargs):
         control.should_save = True
         return control
 
@@ -64,58 +43,35 @@ def token_to_id(tokenizer, max_seq_len):
 
     def get_id(samples: dict) -> dict:
         batch_txt = samples["text"]
-        outputs = tokenizer(
-            batch_txt,
-            padding="max_length",
-            return_attention_mask=False,
-            truncation=True,
-            max_length=max_seq_len
-        )
-        input_ids = [np.array(item, dtype=map_dtype)
-                     for item in outputs["input_ids"]]
+        outputs = tokenizer(batch_txt, padding="max_length", return_attention_mask=False,
+                            truncation=True, max_length=max_seq_len)
+        input_ids = [np.array(item, dtype=map_dtype) for item in outputs["input_ids"]]
         return {"input_ids": input_ids}
     return get_id
 
 
 def get_maped_dataset(files, tokenizer, max_seq_len) -> Dataset:
-    dataset = load_dataset(
-        path="parquet",
-        data_files=files,
-        split="train",
-        cache_dir=TEMP_ROOT + "data/.cache",
-        keep_in_memory=False,
-    )
-    maped_dataset = dataset.map(
-        token_to_id(tokenizer, max_seq_len),
-        batched=True,
-        batch_size=128,
-        remove_columns=dataset.column_names,
-        num_proc=24,
-        keep_in_memory=False,
-    )
+    dataset = load_dataset(path="parquet", data_files=files, split="train",
+                           cache_dir=TEMP_ROOT + "data/.cache", keep_in_memory=False)
+    maped_dataset = dataset.map(token_to_id(tokenizer, max_seq_len), batched=True, batch_size=128,
+                                remove_columns=dataset.column_names, num_proc=24, keep_in_memory=False)
     return maped_dataset
 
 
 if __name__ == "__main__":
     pretrain_config = MiniPhi3PreTrainConfig()
-    config = configuation_miniPhi3.MiniPhiConfig(
-        attn_implementation=pretrain_config.attn_implementation,
-        use_cope=False)
+    config = configuation_miniPhi3.MiniPhiConfig(attn_implementation=pretrain_config.attn_implementation, use_cope=False)
     model = modeling_miniphi3.MiniPhi3(config)
     print(model)
-    tokenizer_path = PROJECT_ROOT+"/tokenizer/"
+    tokenizer_path = PROJECT_ROOT + "/tokenizer/"
     tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
     vocab_size = len(tokenizer)
 
     trainer_callback = miniPhi3TrainerCallback()
-
-    # `mlm=False`表示要训练CLM模型，`mlm=True`表示要训练MLM模型
     data_collator = DataCollatorForLanguageModeling(tokenizer, mlm=False)
 
-    train_dataset = get_maped_dataset(
-        pretrain_config.train_files, tokenizer, pretrain_config.max_seq_len)
-    eval_dataset = get_maped_dataset(
-        pretrain_config.eval_files, tokenizer, pretrain_config.max_seq_len)
+    train_dataset = get_maped_dataset(pretrain_config.train_files, tokenizer, pretrain_config.max_seq_len)
+    eval_dataset = get_maped_dataset(pretrain_config.eval_files, tokenizer, pretrain_config.max_seq_len)
 
     train_args = TrainingArguments(
         output_dir=pretrain_config.output_dir,
@@ -133,7 +89,7 @@ if __name__ == "__main__":
         save_strategy="steps",
         save_total_limit=pretrain_config.keep_latest_n_ckp,
         report_to="tensorboard",
-        optim="adamw_torch",
+        optim=pretrain_config.optim,          # 使用配置中的优化器
         lr_scheduler_type="cosine",
         bf16=True,
         logging_steps=20,
@@ -153,7 +109,6 @@ if __name__ == "__main__":
     )
     trainer.train(resume_from_checkpoint=True)
     eval_results = trainer.evaluate()
-
     print(f"Perplexity: {np.exp(eval_results['eval_loss']):.2f}")
     trainer.save_model(pretrain_config.model_file)
 
